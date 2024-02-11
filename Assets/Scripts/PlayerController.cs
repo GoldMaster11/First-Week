3c9db29ff625e67fe1f88baa4affd27dece2c55b
GoldMaster11 <andriauskaspavel5@yandex.ru>
2024-02-11 13:21:45 +0300

    itog first week

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float gravity = 9.8f;
     public float jumpForce;
+    public float speed;
 
+    private Vector3 _moveVector;
     private float _fallVelocity = 0;
 
     private CharacterController _characterController;
@@ -19,6 +21,25 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
         {
             _fallVelocity = -jumpForce;
@@ -27,7 +48,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+       _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+
         _fallVelocity += gravity * Time.fixedDeltaTime;
-       _characterController.Move(Vector3.down *  _fallVelocity * Time.fixedDeltaTime);  
+       _characterController.Move(Vector3.down *  _fallVelocity * Time.fixedDeltaTime); 
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
     }
 }
