2d9a47802d8664e65279210e507d1dddac02938b
GoldMaster11 <andriauskaspavel5@yandex.ru>
2024-02-11 11:40:52 +0300

    Padenie

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _fallVelocity = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       GetComponent<CharacterController>().Move(Vector3.down *  _fallVelocity * Time.deltaTime);  
+    }
+}
