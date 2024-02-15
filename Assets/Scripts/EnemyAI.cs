dae4ccc9998a0c524c99b7eab2035bd56c25abc3
GoldMaster11 <andriauskaspavel5@yandex.ru>
2024-02-15 21:40:06 +0300

    week 2: idet po traektorii

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,23 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform targetPoint;
+    public List<Transform> patrolPoints;
 
     private NavMeshAgent _navMeshAgent;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+
+    private void InitComponentLinks()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        _navMeshAgent.destination = targetPoint.position;
+        PatrolUpdate();
+    }
+
+    private void PatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoint();
+        }
+
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
     }
 }
