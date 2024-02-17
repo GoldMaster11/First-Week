ccab2b2d0c46b2231675f080e18a89a94f5f2a8d
GoldMaster11 <andriauskaspavel5@yandex.ru>
2024-02-17 21:45:23 +0300

    week 3: fireball letit

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+}
