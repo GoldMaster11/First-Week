9a32e95b124d9a0274bc7cc5ad0fd227cb6b2e3d
GoldMaster11 <andriauskaspavel5@yandex.ru>
2024-03-09 23:00:27 +0300

    2

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,6 +8,8 @@ public class EnemyAI : MonoBehaviour
     public PlayerController player;
     public float viewAngle;
     public float damage = 30;
+    public float attackDistance = 1;
+    public Animator animator;
 
     private NavMeshAgent _navMeshAgent;
     private bool _isPlayerNoticed;
@@ -41,16 +43,25 @@ public class EnemyAI : MonoBehaviour
         {
             if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
             {
-                _playerHealth.DealDamage(damage * Time.deltaTime);
+                animator.SetTrigger("attack");
             }
         }
     }
 
+    public void AttackDamage()
+    {
+        if (!_isPlayerNoticed) return;
+        if (_navMeshAgent.remainingDistance > (_navMeshAgent.stoppingDistance + attackDistance)) return;
+
+        _playerHealth.DealDamage(damage);
+    }
+
     private void NoticePlayerUpdate()
     {
+        _isPlayerNoticed = false;
+        if (!_playerHealth.IsAlive()) return;
 
         var direction = player.transform.position - transform.position;
-        _isPlayerNoticed = false;
         if (Vector3.Angle(transform.forward, direction) < viewAngle)
         {
             RaycastHit hit;
